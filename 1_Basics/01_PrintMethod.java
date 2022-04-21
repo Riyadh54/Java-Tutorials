45d5c2a47c2a62440f08f9e7bc64bf39e63d644d
Merhoun Riyadh <101109154+Riyadh54@users.noreply.github.com>
2022-04-21 23:46:27 +0100

    Add files via upload

---
+++ 1_Basics/01_PrintMethod.java
@@ -0,0 +1,16 @@
+/*
+ */
+package gradleproject2;
+
+/**
+ *
+ * @author Riyadh54
+ */
+public class PrintMethod {
+  
+  public static void main(String[] args) {
+    System.out.println("Learn Java with ease");
+  }
+
+    
+}
