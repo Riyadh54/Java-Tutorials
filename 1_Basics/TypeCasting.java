d63c9032fd241ba79a806460e5f5a61940ef1593
Merhoun Riyadh <101109154+Riyadh54@users.noreply.github.com>
2022-04-22 00:07:56 +0100

    Add files via upload

---
+++ 1_Basics/TypeCasting.java
@@ -0,0 +1,19 @@
+
+package gradleproject2;
+
+/**
+ *
+ * @author Riyadh95
+ */
+public class TypeCasting {
+    public static void main(String[] args){
+          double myDouble = 9.46d;
+          int myInt = (int) myDouble; // Manual casting: double to int
+          // myInt = 9;
+          
+          float myFloat = 6.21f;
+          int number = (int) myFloat;
+          // number = 6;
+    }
+    
+}
