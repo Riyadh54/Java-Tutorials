15f076d73a57ed157178723d73769cc7b85b1d20
Merhoun Riyadh <101109154+Riyadh54@users.noreply.github.com>
2022-04-21 23:54:52 +0100

    Add files via upload

---
+++ 1_Basics/Comments.java
@@ -0,0 +1,18 @@
+
+
+/**
+ *
+ * @author Riyadh95
+ */
+public class Comments {
+    public static void main(String[] args){
+        // This is a comment
+        
+       /*
+        multi-line comment
+        */
+        System.out.println("Hello World"); // This is a comment
+    
+    }
+    
+}
