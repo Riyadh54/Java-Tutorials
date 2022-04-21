0b2c1f7320868110b782a3eff3e422264f238005
Merhoun Riyadh <101109154+Riyadh54@users.noreply.github.com>
2022-04-22 00:19:51 +0100

    Add files via upload

---
+++ 1_Basics/ArithmeticOperators.java
@@ -0,0 +1,32 @@
+
+package gradleproject2;
+
+/**
+ *
+ * @author Riyadh95
+ */
+public class ArithmeticOperators {
+     public static void main(String[] args){
+        
+    int x = 10;
+    int y = 12;
+    
+    int z = y+x; // output 22
+    int u = y-x; // output 2
+    // % Modulus Returns the division remainder	x % y
+    int b = y%x;  // output 2
+    
+   //*	Multiplication	Multiplies two values	x * y
+   int h = x*y; // output 120
+   
+   // ++ Increment Increases the value of a variable by 1 ++x
+   // -- Decrement Decreases the value of a variable by 1 --x	
+   ++x; // output 11
+   --x; // output 10
+        
+       
+    
+    
+    }
+    
+}
