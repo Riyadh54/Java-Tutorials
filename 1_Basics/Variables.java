13aa3476dcbdddf7e032e23a948bf85623d36fc2
Merhoun Riyadh <101109154+Riyadh54@users.noreply.github.com>
2022-04-22 00:02:34 +0100

    Add files via upload

---
+++ 1_Basics/Variables.java
@@ -0,0 +1,18 @@
+
+package gradleproject2;
+
+/**
+ *
+ * @author Riyadh95
+ */
+public class Variables {
+    public static void main (String[] args){
+    
+    int myNum = 25;                  // Primitive data types 
+    float myFloatNum = 6.23f;      // Primitive data types 
+    char myLetter = 'R';          // Primitive data types 
+    boolean myBool = true;       // Primitive data types 
+    String myText = "Hello";    //Non-primitive data types
+    }
+    
+}
